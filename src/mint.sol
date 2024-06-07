3b13c92dbdb2c7f00a3280a946e3f5b626b82936
尹雪剑 <gony952701@gmail.com>
2024-06-07 17:25:13 +0800

    feat: 增加批量mint

--- src/mint.sol
+++ src/mint.sol
@@ -3,21 +3,59 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-contract FakeEfrogsNFT is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+contract FakeEfrogsNFT is ERC721, ERC721URIStorage, ERC721Enumerable, Ownable {
+    uint256 private _tokenIdCounter;
     // 定义一个事件，当NFT铸造成功时触发
     event NFTMinted(uint256 indexed tokenId, string tokenURI);
     event NFTBurned(uint256 indexed tokenId);
     constructor(
         string memory name,
         string memory symbol
-    ) ERC721(name, symbol) Ownable(msg.sender) {}
+    ) ERC721(name, symbol) Ownable(msg.sender) {
+        _tokenIdCounter = 1;
+    }
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal virtual override(ERC721, ERC721Enumerable) {
+        return super._increaseBalance(account, value);
+    }
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal virtual override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+    function totalSupply()
+        public
+        view
+        override(ERC721Enumerable)
+        returns (uint256)
+    {
+        return super.totalSupply();
+    }
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(ERC721, ERC721URIStorage, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
     function safeMint(address to, string memory _tokenURI) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, _tokenURI);
 
@@ -29,7 +67,7 @@ contract FakeEfrogsNFT is ERC721, Ownable {
         string memory _tokenURI
     ) public onlyOwner {
         require(
-            _exists(tokenId),
+            _ownerOf(tokenId) != address(0),
             "ERC721Metadata: URI set of nonexistent token"
         );
         _setTokenURI(tokenId, _tokenURI);
@@ -43,10 +81,8 @@ contract FakeEfrogsNFT is ERC721, Ownable {
         }
     }
     function burn(uint256 tokenId) public {
-        require(
-            _isApprovedOrOwner(msg.sender, tokenId),
-            "Caller is not owner nor approved"
-        );
+        address owner = ownerOf(tokenId);
+        _checkAuthorized(owner, msg.sender, tokenId);
         _burn(tokenId);
         emit NFTBurned(tokenId);
     }
